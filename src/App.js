a8f0033a8c63ed1a8ddfa56dc1bff9220dfcf0dc
jihyeon <kth083030@naver.com>
2024-04-02 00:28:14 +0900

    폰북프로젝트생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 1. 왼쪽 - 연락처 등록하는 폼 / 오른쪽 - 연락처 리스트와 검색창
+// 2. 리스트에 유저 이름과 전화번호를 추가할 수 있다.
+// 3. 리스트에 아이템이 몇개 있는지 보인다.
+// 4. 사용자가 유저를 이름 검색으로 찾을 수 있다.
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
